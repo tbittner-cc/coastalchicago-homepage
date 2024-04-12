a1d5317509a665790d3d729b0c5bfe14a4885c92
tbittner-cc <166669036+tbittner-cc@users.noreply.github.com>
2024-04-12 16:29:41 -0500

    Added a base property to allow for proper asset rendering.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://coastalchicago.com'
+  site: 'https://coastalchicago.com',
+  base: '/'
 });
